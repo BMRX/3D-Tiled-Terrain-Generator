6ada0f8fbb61b471232208d001dadf17be927a21
Michael Bergstrom <mike.b.bergstrom@gmail.com>
2020-08-07 08:03:25 -0700

    Lake removal

--- Assets/homebrew-gen/Grid.cs
+++ Assets/homebrew-gen/Grid.cs
@@ -1,73 +1,92 @@
 ﻿using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 using UnityEngine;
 
 public class Cell {
-    public int x { get; set; }
-    public int y { get; set; }
-    public int id { get; set; }
-    public int score { get; set; }
-    public int type { get; set; }
-    public bool walkable { get; set; }
-    public bool visted { get; set; }
+    public Vector2Int XY { get; set; }
+    public int ID { get; set; }
+    public int Score { get; set; }
+    public int Type { get; set; }
+    public bool Walkable { get; set; }
+    public bool Visited { get; set; }
+
+    public Cell(Vector2Int XY, int ID, int Score, int Type, bool Walkable, bool Visited) {
+        this.XY=XY;
+        this.ID=ID;
+        this.Score=Score;
+        this.Type=Type;
+        this.Walkable=Walkable;
+        this.Visited=Visited;
+    }
 }
 public class Grid {
-    public int[,] grid;
+    public Cell[,] grid;
 
     // Grid dimensions
-    private int xPos; // Width (ex: 250)
-    private int yPos; // Height (ex: 250)
-
-    // I dunno where to put these, used for FloodFill() and FindGroups()
-    List<Cell> visitedCells = new List<Cell>();
-    List<Cell> cellGroup = new List<Cell>();
+    public int Width; // Width (ex: 250)
+    public int Height; // Height (ex: 250)
 
     public Grid(int x, int y) {
-        xPos=x;
-        yPos=y;
+        Width=x;
+        Height=y;
 
-        grid=new int[xPos, yPos];
+        grid=new Cell[Width, Height];
 
-        for (int i = 0; i<xPos; i++) {
-              for (int k = 0; k < yPos; k++) {
-                grid[i, k]=0;
-              }
-          }
-        
+        int cellID = 0;
+        Vector2Int XY;
+
+        for (int i = 0; i<Width; i++) {
+            for (int k = 0; k<Height; k++) {
+                XY=new Vector2Int(i, k);
+                grid[i, k]=new Cell(XY, cellID, 0, 0, false, false);
+                cellID++;
+            }
+        }
+
     }
 
     //set a cell in grid space
-    public void setCell(int x, int y, int v) {
+    public void setCellType(int x, int y, int v) {
         try {
-            grid[x, y]=v;
+            grid[x, y].Type=v;
         } catch {
             Debug.Log("ERROR");
-            Debug.Log("w: "+xPos);
-            Debug.Log("h: "+yPos);
+            Debug.Log("w: "+Width);
+            Debug.Log("h: "+Height);
             Debug.Log("x: "+x);
         }
     }
 
-    public int getCell(int x, int y) {
-        if (x>=0&&x<=xPos-1&&y>=0&&y<=yPos-1) {
-            return grid[x, y];
+    public int getCellType(int x, int y) {
+        if (x>=0&&x<=Width-1&&y>=0&&y<=Height-1) {
+            return grid[x, y].Type;
         } else {
             return -1;
         }
     }
 
+    public Cell GetCell(int x, int y) {
+        if (x>=0&&x<=Width-1&&y>=0&&y<=Height-1) {
+            return grid[x, y];
+        } else {
+            return null;
+        }
+    }
+
     public int getX() {
-        return xPos;
+        return Width;
     }
 
     public int getY() {
-        return yPos;
+        return Height;
     }
 
-    public static int[,] GenerateNoiseMap(int[,] mapGrid, int seed, float scale, int octaves, float persistance, float lacunarity) {
+    public static Cell[,] GenerateNoiseMap(Cell[,] mapGrid, int seed, float scale, int octaves, float persistance, float lacunarity) {
         System.Random prng = new System.Random(seed);
         Vector2[] octaveOffsets = new Vector2[octaves];
+
         for(int i = 0; i < octaves; i ++) {
             float offsetX = prng.Next(-100000,100000);
             float offsetY = prng.Next(-100000,100000);
@@ -106,65 +125,64 @@ public class Grid {
                 } else if(noiseHeight < minNoiseHeight) {
                     minNoiseHeight=noiseHeight;
                 }
-                mapGrid[x, y]=Mathf.RoundToInt(noiseHeight);
+                
+                mapGrid[x, y].Type=Mathf.RoundToInt(noiseHeight);
             }
         }
 
         for (int x = 0; x<mapGrid.GetUpperBound(0); x++) {
             for (int y = 0; y<mapGrid.GetUpperBound(1); y++) {
-                mapGrid[x, y]=Mathf.RoundToInt(Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, mapGrid[x, y]));
+                mapGrid[x, y].Type=Mathf.RoundToInt(Mathf.InverseLerp(minNoiseHeight, maxNoiseHeight, mapGrid[x, y].Type));
+
             }
         }
 
         return mapGrid;
     }
 
-    static int GetMooreSurroundingTiles(int[,] mapGrid, int x, int y) {
+    // I'm bad at math and I cannot for the life of me figure out how to get the noise gen to work properly.
+    public static Cell[,] FuckingLazyConvert(Cell[,] mapGrid) {
+        for (int x = 0; x<mapGrid.GetUpperBound(0); x++) {
+            for (int y = 0; y<mapGrid.GetUpperBound(1); y++) {
+                if(mapGrid[x,y].Type == 1) {
+                    mapGrid[x, y].Type = 2;
+                }
+            }
+        }
+        return mapGrid;
+    }
+
+    static int GetMooreSurroundingTiles(Cell[,] mapGrid, int x, int y) {
         int tileCount = 0;
         for (int neighbourX = x-1; neighbourX<=x+1; neighbourX++) {
             for (int neighbourY = y-1; neighbourY<=y+1; neighbourY++) {
                 if (neighbourX>=0&&neighbourX<mapGrid.GetUpperBound(0)&&neighbourY>=0&&neighbourY<mapGrid.GetUpperBound(1)) {
                     //We don't want to count the tile we are checking the surroundings of
                     if (neighbourX!=x||neighbourY!=y) {
-                        tileCount+=mapGrid[neighbourX, neighbourY];
+                        tileCount+=mapGrid[neighbourX, neighbourY].Type;
                     }
                 }
             }
         }
         return tileCount;
     }
-    public static int[,] SmoothMooreCellularAutomata(int[,] mapGrid, int smoothCount) {
+
+    public static Cell[,] SmoothMooreCellularAutomata(Cell[,] mapGrid, int smoothCount) {
         for (int i = 0; i<smoothCount; i++) {
             for (int x = 0; x<mapGrid.GetUpperBound(0); x++) {
                 for (int y = 0; y<mapGrid.GetUpperBound(1); y++) {
                     int surroundingTiles = GetMooreSurroundingTiles(mapGrid, x, y);
 
                     if (surroundingTiles>4) {
-                        mapGrid[x, y]=1;
+                        mapGrid[x, y].Type=1;
                     } else if (surroundingTiles<4) {
-                        mapGrid[x, y]=0;
+                        mapGrid[x, y].Type=0;
                     }
                 }
             }
         }
         return mapGrid;
     }
-
-    public static int[,] GenerateFalloffMap(int[,] mapGrid) {
-        int[,] falloffMap = new int[mapGrid.GetUpperBound(0),mapGrid.GetUpperBound(1)];
-        for (int i = 0; i<mapGrid.GetUpperBound(0); i++) {
-            for (int j = 0; j<mapGrid.GetUpperBound(1); j++) {
-                
-            }
-        }
-
-        for (int x = 0; x < mapGrid.GetUpperBound(0); x++) {
-            for(int y = 0; y < mapGrid.GetUpperBound(1); y++) {
-                mapGrid[x, y]=mapGrid[x, y]-falloffMap[x, y];
-            }
-        }
-        return mapGrid;
-     }
 
     public float GetDistance(int _x, int _y, Vector3 target) {
         Vector3 thisTile = new Vector3(_x,0,_y);
@@ -172,58 +190,44 @@ public class Grid {
         return distance;
     }
 
-    public void EvaluateCells(List<Cell> cells) {
-        int cellId = 0;
-        // Get all cells and add them to cell list
-        for (int x = 0; x<getX(); x++) {
-            for (int y = 0; y<getY(); y++) {
-                cellId++;
-                Cell newCell = new Cell();
-                newCell.x=x;
-                newCell.y=y;
-                newCell.id=cellId;
-                newCell.type=getCell(x, y);
-                cells.Add(newCell);
-            }
-        }
-    }
-    //step 1: create a two-dimensional array to represent the cells you've been to already, and the cells you're still considering; set everything to unconsidered except for the selected cell
-    //step 2: enter a for loop with the exit condition of "no pending cells remaining"
-    //Check each cell for pending status.
-    //If the cell is traversable, set its unconsidered neighbors to pending and itself to considered.
-    //Mark it as either connected or disconnected, depending on whether it's traversable.
+    /*
+        Visualisation of what grid looks like after generation
+
+        0 0 0 0 0 0 0 0 0 0
+        0 0 1 1 0 1 0 0 0 0
+        0 1 0 0 1 0 1 1 1 0
+        0 1 0 1 4 0 1 1 1 0
+        0 0 1 2 2 3 1 0 0 0
+        0 0 0 0 1 0 0 0 1 1
+        0 0 0 0 0 0 0 0 0 0
+
+    */
 
     public List<List<Cell>> FindConnectedGroups(Grid mapGrid, int type) {
-        var groups = new List<List<Cell>>();
+        List<List<Cell>> groups = new List<List<Cell>>();
 
-        // Search the world grid for pairs of connected blocks.
         for (int x = 0; x<mapGrid.getX(); x++) {
             for (int y = 0; y<mapGrid.getY(); y++) {
-                var cell = mapGrid.getCell(x,y);
+                Cell cell = mapGrid.GetCell(x,y);
 
-                // Skip blocks we've already grouped.
-                // If you don't want to add a visited field to every block, 
-                // you can accomplish this with a parallel array instead.
-                if (cell.visited)
+                if (cell.Visited)
                     continue;
 
-                // Remove this check if you want to find groups of any color.
-                if (block.color==matchColor) {
-                    // Every group of 2+ blocks has a block to the right or below another,
-                    // so by checking just these two directions we don't exclude any.
+                if (cell.Type==type) {
                     if (x>0) {
-                        var neighbor = world.GetBlock(x - 1, y);
-                        if (neighbor.color==block.color) {
-                            var group = new List<Block>();
-                            PopulateGroup(world, group, block);
+                        Cell neighbor = mapGrid.GetCell(x - 1, y);
+                        if (neighbor.Type==cell.Type) {
+                            List<Cell> group = new List<Cell>();
+                            PopulateGroup(mapGrid, group, cell);
                             groups.Add(group);
                             continue;
                         }
                     }
+
                     if (y>0) {
-                        var neighbor = mapGrid.GetBlock(x, y - 1);
-                        if (neighbor.color==cell.color) {
-                            var group = new List<Cell>();
+                        Cell neighbor = mapGrid.GetCell(x, y - 1);
+                        if (neighbor.Type==cell.Type) {
+                            List<Cell> group = new List<Cell>();
                             PopulateGroup(mapGrid, group, cell);
                             groups.Add(group);
                         }
@@ -232,53 +236,63 @@ public class Grid {
             }
         }
 
-        // Set all tiles back to unvisited for the next use.
-        world.ClearVisitedFlags();
-
+        mapGrid.ClearVistedState();
         return groups;
     }
 
     // Recursively find connected blocks (depth-first search)
-    void PopulateGroup(World world, List<Block> group, block) {
-        group.Add(block);
-        block.visited=true;
+    private void PopulateGroup(Grid mapGrid, List<Cell> group, Cell cell) {
+        cell.Visited=true;
+        group.Add(cell);
 
-        // Check all four neighbors and recurse on them if needed:
-        if (block.x>0) {
-            var neighbor = world.GetBlock(block.x - 1, block.y);
-            if (neighbor.color==block.color&&neighbour.visited==false)
-                PopulateGroup(world, group, neighbor);
+        if (cell.XY.x>0) {
+            Cell neighbor = mapGrid.GetCell(cell.XY.x - 1, cell.XY.y);
+            if (neighbor.Type==cell.Type && neighbor.Visited==false) {
+                PopulateGroup(mapGrid, group, neighbor);
+            }
         }
-        if (block.x<world.Width-1) {
-            var neighbor = world.GetBlock(block.x + 1, block.y);
-            if (neighbor.color==block.color&&neighbour.visited==false)
-                PopulateGroup(world, group, neighbor);
+        if (cell.XY.x<mapGrid.getX() - 1) {
+            Cell neighbor = mapGrid.GetCell(cell.XY.x + 1, cell.XY.y);
+            if (neighbor.Type==cell.Type && neighbor.Visited==false) {
+                PopulateGroup(mapGrid, group, neighbor);
+            }
         }
-        if (block.y>0) {
-            var neighbor = world.GetBlock(block.x, block.y - 1);
-            if (neighbor.color==block.color&&neighbour.visited==false)
-                PopulateGroup(world, group, neighbor);
+        if (cell.XY.y>0) {
+            Cell neighbor = mapGrid.GetCell(cell.XY.x, cell.XY.y - 1);
+            if (neighbor.Type==cell.Type && neighbor.Visited==false) {
+                PopulateGroup(mapGrid, group, neighbor);
+            }
         }
-        if (block.y<world.Height-1) {
-            var neighbor = world.GetBlock(block.x, block.y + 1);
-            if (neighbor.color==block.color&&neighbour.visited==false)
-                PopulateGroup(world, group, neighbor);
+        if (cell.XY.y<mapGrid.getY() - 1) {
+            Cell neighbor = mapGrid.GetCell(cell.XY.x, cell.XY.y + 1);
+            if (neighbor.Type==cell.Type && neighbor.Visited==false) {
+                PopulateGroup(mapGrid, group, neighbor);
+            }
         }
+    }
+    
+    private void ClearVistedState() {
+        for(int x = 0; x < getX(); x++) {
+            for (int y = 0; y<getX(); y++) {
+                grid[x, y].Visited=false;
+            }
+        }
+    }
+
+    public static Grid RemoveWater(List<List<Cell>> groups, Grid mapGrid, int count) {
+
+        for (int i = 0; i<count; i++) {
+            int index = UnityEngine.Random.Range(0, groups.Count);
+            var group = groups[index];
+            for (int k =0; k<group.Count; k++) {
+                mapGrid.setCellType(group[k].XY.x, group[k].XY.y, 1);
+            }
+        }
+
+        return mapGrid;
     }
 
     // Painting Methods
-    public void FloodFill(int x, int y, int fill, int old) {
-        if ((x<0)||(x>=getX())) return;
-        if ((y<0)||(y>=getY())) return;
-        if (getCell(x, y)==old) {
-            setCell(x, y, fill);
-            FloodFill(x+1, y, fill, old);
-            FloodFill(x, y+1, fill, old);
-            FloodFill(x-1, y, fill, old);
-            FloodFill(x, y-2, fill, old);
-        }
-    }
-
     public void CircleEraser(int _x, int _y, int type,float range) {
         Vector3 target = new Vector3(_x,0,_y);
         for (int x = 0; x<getX(); x++) {
@@ -286,10 +300,10 @@ public class Grid {
                 if (GetDistance(x, y, target)<=range) {
                     switch (type) {
                         case 1:
-                            setCell(x, y, 0);
+                            setCellType(x, y, 0);
                         break;
                         case 2:
-                            setCell(x, y, 1);
+                            setCellType(x, y, 1);
                         break;
                         case 0:
                         // nothing
@@ -300,22 +314,23 @@ public class Grid {
         }
     }
 
-    public void OuterCircleEraser(int _x, int _y, int type, float range) {
-        Vector3 target = new Vector3(_x,0,_y);
+    public void EdgeEraser() {
         for (int x = 0; x<getX(); x++) {
             for (int y = 0; y<getY(); y++) {
-                if (GetDistance(x, y, target)>=range) {
-                    switch (type) {
-                        case 1:
-                        setCell(x, y, 0);
-                        break;
-                        case 2:
-                        setCell(x, y, 1);
-                        break;
-                        case 0:
-                        // nothing
-                        break;
-                    }
+                if (x==0) {
+                    setCellType(x, y, 0);
+                }
+
+                if (x==getX()-1) {
+                    setCellType(x, y, 0);
+                }
+
+                if (y==0) {
+                    setCellType(x, y, 0);
+                }
+
+                if (y==getX()-1) {
+                    setCellType(x, y, 0);
                 }
             }
         }
